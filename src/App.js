faf21a9063a75821ee1334503ca31784cdf4df57
Navdeepsingh4298 <navdeepsingh4298@gmail.com>
2022-09-16 13:56:14 +0530

    add item list header filter and search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ItemListHead from "./components/item-list-head/item.list.head.component";
+
+// Stylesheet
+import "./App.css";
+
 function App() {
 	return (
 		<>
-			<div>Hello</div>
+			<ItemListHead />
 		</>
 	);
 }
